2269d164a5f27ef054f3dc516689b1c20354e722
admodev <admosdigital@yahoo.com>
2020-11-15 12:44:51 -0300

    add colored output, crate: colour v0.5.0

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use ::std::*;
 
+#[macro_use]
+extern crate colour;
+
 fn main() {
     let mut name = String::new();
     println!("Hello there");
     println!("Please enter your name: ");
     let read_name = std::io::stdin().read_line(&mut name).unwrap();
     println!("Hello, {}", name);
-    println!("Welcome to mydia!");
+    cyan_ln!("Welcome to mydia!");
 }
