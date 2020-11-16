c9924253971d27961033b87a0ccabeaf5b2f473a
admodev <admosdigital@yahoo.com>
2020-11-16 09:16:06 -0300

    add console crate, created selector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use ::std::*;
 use colorful::Color;
 use colorful::Colorful;
+use dialoguer::{
+    Select,
+    theme::ColorfulTheme
+};
+use console::Term;
 
 #[macro_use]
 extern crate colorful;
+
+fn select_menu() -> std::io::Result<()> {
+    let items = vec!["Today's goal", "Write"];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())?;
+
+    match selection {
+        Some(index) => println!("User selected item : {}", items[index]),
+        None => println!("User did not select anything")
+    }
+
+    Ok(())
+}
 
 fn main() {
     let mut name = String::new();
@@ -12,4 +32,6 @@ fn main() {
     let read_name = std::io::stdin().read_line(&mut name).unwrap();
     println!("Hello, {}", name.gradient(Color::Red));
     println!("{}", "Welcome to mydia!".gradient(Color::Green)); 
+
+    select_menu();
 }
