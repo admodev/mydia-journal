c28b7477dbeca66ed08517944a89d6e9a786c48c
admodev <admosdigital@yahoo.com>
2020-11-16 08:56:11 -0300

    change colored intro message output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use ::std::*;
+use colorful::Color;
+use colorful::Colorful;
 
 #[macro_use]
-extern crate colour;
+extern crate colorful;
 
 fn main() {
     let mut name = String::new();
     println!("Hello there");
     println!("Please enter your name: ");
     let read_name = std::io::stdin().read_line(&mut name).unwrap();
-    println!("Hello, {}", name);
-    cyan_ln!("Welcome to mydia!");
+    println!("Hello, {}", name.gradient(Color::Red));
+    println!("{}", "Welcome to mydia!".gradient(Color::Green)); 
 }
